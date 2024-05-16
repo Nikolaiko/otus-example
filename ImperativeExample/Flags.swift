72d33610806be72e461e12199d06f8639a88c53a
Nikolai Baklanov <nikolaiko@mail.ru>
2024-05-17 00:00:57 +0600

    Initial

---
+++ ImperativeExample/Flags.swift
@@ -0,0 +1,10 @@
+//
+//  Flags.swift
+//  ImperativeExample
+//
+//  Created by Nikolai Baklanov on 16.05.2024.
+//
+
+import Foundation
+
+let exampleFlag = false
---
+++ ImperativeExample/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var unicronImage: UIImageView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func onButtonShowClick() {
+        if exampleFlag {
+            actionButton.tintColor = UIColor.yellow
+        } else {
+            actionButton.tintColor = UIColor.red
+        }
+        actionButton.setTitle("Уверены?", for: .normal)
+        actionButton.setTitle("Уверены?", for: .highlighted)
+
+        actionButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(secondClick)))
+    }
+
+    @objc
+    func secondClick() {
+        unicronImage.isHidden = false
+    }
+}
+
